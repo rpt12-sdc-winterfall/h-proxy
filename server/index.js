b0e832415cedfa0c7a4fcf79eb776eb4c2cdb9f8
heather <heatherlynn1337@gmail.com>
2019-06-18 13:05:24 -0700

    adding changes to server/public to setup working proxy

--- server/index.js
+++ server/index.js
@@ -1,13 +1,29 @@
 /* eslint-disable import/no-extraneous-dependencies */
+require('newrelic');
 const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
+// const morgan = require('morgan');
+// const cors = require('cors');
+// const bodyParser = require('body-parser');
+const httpProxy = require('http-proxy');
 
-const port = process.env.PORT || 3000;
+//Creating connection to Proxy
 const app = express();
-app.listen(port, () => console.log(`listening on port ${port}`));
+const proxy = httpProxy.createProxyServer();
+const port = process.env.PORT || 3009;
+
+// app.listen(port, () => console.log(`listening on port ${port}`));
 
 app.use('/:id', express.static(`${__dirname}/../public`));
 app.use(express.static(`${__dirname}/../public`));
-app.use(morgan('dev'));
-app.use(cors());
+// app.use(morgan('dev'));
+// app.use(cors());
+
+//To reviews service (Heather)
+app.all('/reviews/:id', (req, res) => {
+    proxy.web(req, res, {target: 'http:localhost:3008' });
+});
+
+
+
+
+app.listen(port, () => console.log(`Proxy server listening on port ${port}`));
